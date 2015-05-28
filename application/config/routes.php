0b866da83c03e2f730621d1c3ac807a6522e3730
Martín Lucas Golini <spartanj@gmail.com>
2015-05-28 16:26:10 -0300

    Handle URLs without controller.

--- application/config/routes.php
+++ application/config/routes.php
@@ -39,7 +39,7 @@
 */
 
 $route['default_controller']	= 'blog';
-$route['404_override']			= '';
+$route['404_override']			= 'error';
 $route['blog/(:any)']			= "blog/show/$1";
 $route['section/(:any)']		= "blog/show/$1";
 
--- application/views/frame/header.php
+++ application/views/frame/header.php
@@ -2,7 +2,7 @@
 <div id="bar">
 	<div class="menu">
 		<ul	id="links">
-			<li><a class="ajax-link" href="<?=base_url('/section/que-podemos-hacer-por-vos')?>"><?=lang_line('about_us')?></a></li>
+			<li><a class="ajax-link" href="<?=base_url('/que-podemos-hacer-por-vos')?>"><?=lang_line('about_us')?></a></li>
 			<li><a class="ajax-link" href="<?=base_url('/blog')?>"><?=lang_line('blog')?></a></li>
 			<li><a class="ajax-link" href="<?=base_url('/contact')?>"><?=lang_line('contact')?></a></li>
 		</ul>
