fe8f930d63f91de9f12cba3c069cfacb3db470b3
Martín Lucas Golini <spartanj@gmail.com>
2015-05-29 16:19:33 -0300

    table prefix support.

--- application/models/Categories_model.php
+++ application/models/Categories_model.php
@@ -2,30 +2,38 @@
 
 class Categories_model extends CI_Model
 {
+	protected $table_name = 'categories';
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->table_name = $this->db->dbprefix . $this->table_name;
+	}
+	
 	function add( $key, $name )
 	{
-		$this->db->query("INSERT INTO categories (cat_key, cat_name) VALUES (?,?)", array( $key, $name ) );
+		$this->db->query("INSERT INTO {$this->table_name} (cat_key, cat_name) VALUES (?,?)", array( $key, $name ) );
 		
 		return $this->db->insert_id();
 	}
 	
 	function update( $key, $name, $id )
 	{
-		$this->db->query("UPDATE categories SET cat_key = ?, cat_name = ? WHERE cat_id = ?", array( $key, $name, $id ) );
+		$this->db->query("UPDATE {$this->table_name} SET cat_key = ?, cat_name = ? WHERE cat_id = ?", array( $key, $name, $id ) );
 	}
 	
 	function get_all()
 	{
-		return $this->db->query("SELECT * FROM categories ORDER BY cat_name ASC")->result_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} ORDER BY cat_name ASC")->result_array();
 	}
 	
 	function get( $id )
 	{
-		return $this->db->query("SELECT * FROM categories WHERE cat_id = ? LIMIT 1", array( $id ) )->row_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} WHERE cat_id = ? LIMIT 1", array( $id ) )->row_array();
 	}
 	
 	function get_by_key( $key )
 	{
-		return $this->db->query("SELECT * FROM categories WHERE cat_key = ? LIMIT 1", array( $key ) )->row_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} WHERE cat_key = ? LIMIT 1", array( $key ) )->row_array();
 	}
 }
--- application/models/Posts_model.php
+++ application/models/Posts_model.php
@@ -2,21 +2,29 @@
 
 class Posts_model extends CI_Model
 {
+	protected $table_name = 'posts';
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->table_name = $this->db->dbprefix . $this->table_name;
+	}
+	
 	function add( $data,$slug, $admin_id )
 	{
-		$this->db->query("INSERT INTO posts (post_title, post_body, post_category, post_draft, post_slug, post_admin_id, post_created) VALUES (?,?,?,?,?,?, now())", array( $data['title'], $data['body'], $data['category'], $data['draft'], $slug, $admin_id ) );
+		$this->db->query("INSERT INTO {$this->table_name} (post_title, post_body, post_category, post_draft, post_slug, post_admin_id, post_created) VALUES (?,?,?,?,?,?, now())", array( $data['title'], $data['body'], $data['category'], $data['draft'], $slug, $admin_id ) );
 		
 		return $this->db->insert_id();
 	}
 	
 	function update( $data, $slug, $update_timestamp = FALSE )
 	{
-		$this->db->query("UPDATE posts SET post_title = ?, post_body = ?, post_category = ?, post_draft = ?, post_slug = ?, post_updated = NOW() WHERE post_id = ?", array( $data['title'], $data['body'], $data['category'], $data['draft'], $slug, $data['post_id'] ) );
+		$this->db->query("UPDATE {$this->table_name} SET post_title = ?, post_body = ?, post_category = ?, post_draft = ?, post_slug = ?, post_updated = NOW() WHERE post_id = ?", array( $data['title'], $data['body'], $data['category'], $data['draft'], $slug, $data['post_id'] ) );
 	}
 	
 	function get_drafts()
 	{
-		return $this->db->query("SELECT * FROM posts INNER JOIN categories ON post_category = cat_id WHERE post_draft = 1 ORDER BY post_created DESC")->result_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} INNER JOIN {$this->db->dbprefix}categories ON post_category = cat_id WHERE post_draft = 1 ORDER BY post_created DESC")->result_array();
 	}
 
 	function get_published( $category = NULL )
@@ -28,7 +36,7 @@ class Posts_model extends CI_Model
 			$and = "AND post_category = ?";
 		}
 		
-		return $this->db->query("SELECT * FROM posts INNER JOIN categories ON post_category = cat_id WHERE post_draft = 0 " . $and . " ORDER BY post_created DESC", array( $category ) )->result_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} INNER JOIN {$this->db->dbprefix}categories ON post_category = cat_id WHERE post_draft = 0 " . $and . " ORDER BY post_created DESC", array( $category ) )->result_array();
 	}
 	
 	function get_published_by_category_key( $cat_key = NULL )
@@ -40,7 +48,7 @@ class Posts_model extends CI_Model
 			$and = "AND cat_key = ?";
 		}
 		
-		return $this->db->query("SELECT * FROM posts INNER JOIN categories ON post_category = cat_id WHERE post_draft = 0 " . $and . " ORDER BY post_created DESC", array( $cat_key ) )->result_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} INNER JOIN {$this->db->dbprefix}categories ON post_category = cat_id WHERE post_draft = 0 " . $and . " ORDER BY post_created DESC", array( $cat_key ) )->result_array();
 	}
 	
 	function get_rss( $category = NULL )
@@ -50,31 +58,31 @@ class Posts_model extends CI_Model
 			$and = "AND post_category = ?";
 		}
 		
-		return $this->db->query("SELECT * FROM posts INNER JOIN categories ON post_category = cat_id WHERE post_draft = 0 " . $and . " ORDER BY post_created DESC", array( $category ) )->result_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} INNER JOIN {$this->db->dbprefix}categories ON post_category = cat_id WHERE post_draft = 0 " . $and . " ORDER BY post_created DESC", array( $category ) )->result_array();
 	}
 	
 	function get( $post_id )
 	{
-		return $this->db->query("SELECT * FROM posts INNER JOIN categories ON post_category = cat_id WHERE post_id = ?", array( $post_id ) )->row_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} INNER JOIN {$this->db->dbprefix}categories ON post_category = cat_id WHERE post_id = ?", array( $post_id ) )->row_array();
 	}
 
 	function delete( $post_id )
 	{
-		$this->db->query("DELETE FROM posts WHERE post_id = ?", array( $post_id ) );
+		$this->db->query("DELETE FROM {$this->table_name} WHERE post_id = ?", array( $post_id ) );
 	}
 
 	function get_slug($slug)
 	{
-		return $this->db->query("SELECT * FROM posts INNER JOIN categories ON post_category = cat_id WHERE post_slug = ?", array( $slug ) )->row_array();
+		return $this->db->query("SELECT * FROM {$this->table_name} INNER JOIN {$this->db->dbprefix}categories ON post_category = cat_id WHERE post_slug = ?", array( $slug ) )->row_array();
 	}
 	
 	function slug_exists_and_not_me( $slug, $post_id )
 	{
-		return NULL != $this->db->query("SELECT 1 FROM posts WHERE post_slug = ? AND post_id != ? LIMIT 1", array( $slug, $post_id ) )->row_array();
+		return NULL != $this->db->query("SELECT 1 FROM {$this->table_name} WHERE post_slug = ? AND post_id != ? LIMIT 1", array( $slug, $post_id ) )->row_array();
 	}
 	
 	function slug_exists( $slug )
 	{
-		return NULL != $this->db->query("SELECT 1 FROM posts WHERE post_slug = ? LIMIT 1", array( $slug ) )->row_array();
+		return NULL != $this->db->query("SELECT 1 FROM {$this->table_name} WHERE post_slug = ? LIMIT 1", array( $slug ) )->row_array();
 	}
 }
