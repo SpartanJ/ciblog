edecdfa031c82a4dfd8609141d355afc60154626
Mart??n Lucas Golini <spartanj@gmail.com>
2016-07-09 03:26:52 -0300

    Gravatar https.

--- application/helpers/utils_helper.php
+++ application/helpers/utils_helper.php
@@ -975,7 +975,7 @@ class CiblogHelper
 	 */
 	public static function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() )
 	{
-		$url = 'http://www.gravatar.com/avatar/';
+		$url = 'https://www.gravatar.com/avatar/';
 		$url .= md5( strtolower( trim( $email ) ) );
 		$url .= "?s=$s&d=$d&r=$r";
 		if ( $img ) {
