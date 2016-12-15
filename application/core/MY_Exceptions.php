41013c5b47a54dc5be6514c526b24d9fdaec48f4
Martín Lucas Golini <spartanj@gmail.com>
2016-12-14 22:50:02 -0300

    Another fix.

--- application/core/MY_Exceptions.php
+++ application/core/MY_Exceptions.php
@@ -5,6 +5,23 @@ class MY_Exceptions extends CI_Exceptions {
 	{
 		$severity = isset($this->levels[$severity]) ? $this->levels[$severity] : $severity;
 		log_message('error', 'Severity: '.$severity.' --> '.$message.' '.$filepath.' '.$line);
-		log_backtrace();
+		log_message('error', 'Backtrace:' );
+		
+		$backtrace = debug_backtrace();
+		
+		if ( isset( $backtrace ) && !empty( $backtrace ) )
+		{
+			foreach ( $backtrace as $t )
+			{
+				if ( isset($t['file']) )
+				{
+					log_message('error', "file: " . $t['file'] . ":" . $t['line'] . " func: " . $t['function'] );
+				}
+				else
+				{
+					log_message('error', "file: ?:? func: " . $t['function'] );
+				}
+			}
+		}
 	}
 }
